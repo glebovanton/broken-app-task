7977925629c9a8cf2d74cf8d1a55844669d7c802
Anton <workglebov@gmail.com>
2021-05-20 00:05:01 +0300

    Fix `sequelize` in db.js

--- db.js
+++ db.js
@@ -1,17 +1,24 @@
+require('dotenv').config();
 const Sequelize = require('sequelize');
-                                //database username   password
-const sequelize = new Sequelize('gamedb', process.env.USERNAME, process.env.PASSWORD, {
+
+const sequelize = new Sequelize(
+  'gamedb',
+  process.env.USERNAME,
+  process.env.PASSWORD,
+  {
     host: 'localhost',
-    dialect: 'postgres'
-})
+    dialect: 'postgres',
+    port: 5433,
+  }
+);
 
 const sync = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
 
 module.exports = { sequelize, sync };
