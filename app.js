c626fcf1f8c36efcbaf20c4bcb7bba701c60df20
Anton <workglebov@gmail.com>
2021-05-22 09:27:34 +0300

    Refactor app.js

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+const bodyParser = require('body-parser')
 const express = require('express');
-const app = express();
+
 const db = require('./db');
-const user = require('./controllers/usercontroller');
 const game = require('./controllers/gamecontroller');
+const user = require('./controllers/usercontroller');
 
+const app = express();
+const port = 4000
 
-db.sync();
-app.use(require('body-parser'));
+db.sync().then(()=>{
+    app.listen(port,() => {
+        console.log(`App is listening on ${port}`);
+    })
+}).catch(err=>console.log(err));
+app.use(bodyParser.json());
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
+
 app.use('/api/game', game);
-app.listen(function() {
-    console.log("App is listening on 4000");
-})
